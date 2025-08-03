c31a799b14b46ea092241c8ff6e4168fd32d0560
SauravKumar-sys <2023ht66511@wilp.bits-pilani.ac.in>
2025-08-03 20:46:32 +0530

    Update Calculator.py

--- Calculator.py
+++ Calculator.py
@@ -4,21 +4,21 @@ def add(x, y):
     """Adds two numbers."""
     return x + y
 
-def subtract(x, y):
+#def subtract(x, y):
     """Subtracts two numbers."""
     return x - y
 
-#def multiply(x, y):
+def multiply(x, y):
     """Multiplies two numbers."""
     return x * y
 
-#def divide(x, y):
+def divide(x, y):
     """Divides two numbers. Handles division by zero."""
     if y == 0:
         return "Error: Division by zero!"
     return x / y
 
-def power(x, y):
+#def power(x, y):
     """Calculates x raised to the power of y."""
     return x ** y
 
