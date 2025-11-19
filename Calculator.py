421da4c101afea70c7fe34834e2ac2acea7de985
SauravKumar-sys <2023ht66511@wilp.bits-pilani.ac.in>
2025-11-19 21:08:32 +0530

    Uncomment multiply and divide functions

--- Calculator.py
+++ Calculator.py
@@ -8,11 +8,11 @@ def subtract(x, y):
     """Subtracts two numbers."""
     return x - y
 
-#def multiply(x, y):
+def multiply(x, y):
     """Multiplies two numbers."""
     return x * y
 
-#def divide(x, y):
+def divide(x, y):
     """Divides two numbers. Handles division by zero."""
     if y == 0:
         return "Error: Division by zero!"
